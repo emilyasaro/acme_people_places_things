7be22bd41ec08b0125b7c91868f37fae67068a20
Anna Litovskaya <Litovskaya09@gmail.com>
2021-02-13 14:15:06 -0500

    form created

--- db.js
+++ db.js
@@ -1,55 +1,72 @@
 // where we create our tables
 
 const Sequelize = require('sequelize');
-const { STRING } = Sequelize;
+const { STRING, INTEGER, DATE } = Sequelize;
 
-const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme_people_places_things')
+const db = new Sequelize(
+  process.env.DATABASE_URL || 'postgres://localhost/acme_people_places_things'
+);
 
 const Person = db.define('person', {
   name: {
     type: STRING,
     unique: true,
-    allowNull: false
-  }
-})
+    allowNull: false,
+  },
+});
 
-// const Place = db.define('place', {
-//   name: {
-//     type: STRING,
-//     unique: true,
-//     allowNull: false
-//   }
-// })
+const Place = db.define('place', {
+  name: {
+    type: STRING,
+    unique: true,
+    allowNull: false,
+  },
+});
 
-// const Thing = db.define('thing', {
-//   name: {
-//     type: STRING,
-//     unique: true,
-//     allowNull: false
-//   }
-// })
+const Thing = db.define('thing', {
+  name: {
+    type: STRING,
+    unique: true,
+    allowNull: false,
+  },
+});
 
-// const Purchase = db.define('purchase', {
-//   quantity: {
-//     type: INTEGER,
-//     allowNull: false
-//     },
-//   time: {
-//     type: DATE
-//   }
-// })
+const Purchase = db.define('purchase', {
+  quantity: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  date: {
+    type: DATE,
+  },
+});
+
+Person.hasMany(Thing);
+//Thing.belongsToMany(Person, { through: Thing.name });
+Person.hasMany(Place);
+//Place.belongsToMany(Person);
+Purchase.belongsTo(Person);
+Purchase.belongsTo(Thing);
 
 const syncAndSeed = async () => {
   await db.sync({ force: true });
-  const [ moe, lucy, curly ] = await Promise.all(
-    [ 'moe', 'lucy', 'curly' ].map( name => Person.create({ name }))
-  )
-}
+  const [moe, lucy, curly] = await Promise.all(
+    ['moe', 'lucy', 'curly'].map((name) => Person.create({ name }))
+  );
+  const [NYC, Chicago, LA, Dallas] = await Promise.all(
+    ['NYC', 'Chicago', 'LA', 'Dallas'].map((name) => Place.create({ name }))
+  );
+  const [foo, bar, bazz, quq] = await Promise.all(
+    ['foo', 'bar', 'bazz', 'quq'].map((name) => Thing.create({ name }))
+  );
+};
 
 module.exports = {
   syncAndSeed,
   models: {
     Person,
-    // Place, Thing, Purchase
-  }
-}
+    Place,
+    Thing,
+    //Purchase
+  },
+};
--- server.js
+++ server.js
@@ -1,14 +1,106 @@
-const { syncAndSeed, models: { Person} } = require('./db.js');
+const {
+  syncAndSeed,
+  models: { Person, Place, Thing },
+} = require('./db.js');
+const express = require('express');
+const app = express();
+const html = require('html-template-tag');
 
-// , Place, Thing, Purchase
+app.get('/', async (req, res, next) => {
+  try {
+    const [people, things, places] = await Promise.all([
+      Person.findAll(),
+      Thing.findAll(),
+      Place.findAll(),
+    ]);
+    res.send(html`
+      <html>
+        <head>
+          <title>Acme People Places Things</title>
+        </head>
+        <body>
+          <h1>Acme People Places Things</h1>
+          <div>
+            <h3>People (${people.length})</h3>
+            <ul>
+              $${people
+                .map((person) => {
+                  return html` <li>${person.name}</li> `;
+                })
+                .join('')}
+            </ul>
+          </div>
+          <div>
+            <h3>Things (${things.length})</h3>
+            <ul>
+              $${things
+                .map((thing) => {
+                  return html` <li>${thing.name}</li> `;
+                })
+                .join('')}
+            </ul>
+          </div>
+          <div>
+            <h3>Places (${places.length})</h3>
+            <ul>
+              $${places
+                .map((place) => {
+                  return html` <li>${place.name}</li> `;
+                })
+                .join('')}
+            </ul>
+          </div>
+          <form method="POST" action="/purchase/${purchase.id}">
+            <select name="personId">
+              <option>--person--</option>
+              $${people
+                .map((person) => {
+                  return html` <option>${person.name}</option> `;
+                })
+                .join('')}
+            </select>
+            <select name="placeId">
+              <option>--place--</option>
+              $${places
+                .map((place) => {
+                  return html` <option>${place.name}</option> `;
+                })
+                .join('')}
+            </select>
+            <select name="thingId">
+              <option>--thing--</option>
+              $${things
+                .map((thing) => {
+                  return html` <option>${thing.name}</option> `;
+                })
+                .join('')}
+            </select>
+            <input type="number" />
+            <input type="date" />
+            <button>Create Purchase</button>
+          </form>
+          <ul></ul>
+        </body>
+      </html>
+    `);
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+app.post();
+
 const init = async () => {
   try {
-    await syncAndSeed()
+    await syncAndSeed();
+  } catch (ex) {
+    console.log(ex);
   }
-  catch(ex) {
-    console.log(ex)
-  }
-}
+};
 
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`App listening in port: ${PORT}`);
+});
 
 init();
